14e93cffac55902d98d40bdbea604b3ae11faca0
Tristan ALLAIRE <tristan.allaire@alarch.fr>
2024-10-24 11:28:59 +0200

    Indexer updated for Dipdup 8 + Build updated

---
+++ src/contracts/scripts/sandbox/accounts copy.js
@@ -0,0 +1,68 @@
+module.exports = {
+    baker: {
+        pkh: "mv1LkuVrpuEYCjZqTM93ri8aKYNtqFoYeACk",
+        sk: "edsk3RFgDiCt7tWB2oe96w1eRw72iYiiqZPLu9nnEY23MYRp2d8Kkx",
+        pk: "edpkuTpUWcNgn4QYcBVGDLy6rmpJ3WSTSV2bdiJFwyoDk5fSwxyV5k",
+        peerId: "bakerPeerId"
+    },
+    alice: {
+        pkh: "mv1E2Y8khTrfaRUeErWUBfg6G7zNMKnM4JJL",
+        sk: "edskS5Xd6CDBLbuJwkaa7mT2K6mom4odhbBiS3bxDtAB1crXWj4gwKF5oQsN9aijR3CNpr7cZXxcoUU32vqm67W3MRGSTVzMmz",
+        pk: "edpku8CdxqUzHhL8X3fgpCX5CfmqxUU7JWBTmXwqUATt78dGijvqWd",
+        peerId: "12D3KooWK87KmBGJZZMP3keux62VF515mFRbNRFwbYxib7wWQR34"
+    },
+    bob: {
+        pkh: "mv1TMgthRwT69X8WMqRyeMYLPEcoEfCKqX2w",
+        sk: "edskSA1MhTp6Eq3T79MEP822eXAmxXBk89eFYGgwBsJjfyUHDGsYfudasQocwcb5DUEMvA1B3EsvxCZ8G6Wek6syxAA49DEKzq",
+        pk: "edpktqePwpgrWGS49FiAMDacb3bLiDLE8BrbLoi6zYdcZ9bttDLo1D",
+        peerId: "bobPeerId"
+    },
+    eve: {
+        pkh: "mv1Q3DyGiVYDrRj5PrUVQkTA1LHwYy8gHwQV",
+        sk: "edskRvgMBH37Dci9isEHcdsHQ4ioPdUq3AfXDiAj3ZXiuy3YMs4LEUiZVMaSG9KjTFo78LidgZkdVbkXUamMK2or8UAxB743SH",
+        pk: "edpkv758sKsFGRwE3ZNDR1wdVEnxZui344fsxEtCKbpNMcTaQw2Yis",
+        peerId: "12D3KooWBpgAXhUAgjPAwEk5FJ9DRB2kFbuj8KLkPPmqKKmzrXz2"
+    },
+    mallory: {
+        pkh: "mv1Ffw8ejzFRH6zBtbMdB87qzf5MZBhr8e3L",
+        sk: "edsk3bVbowf9hFpdk8mAjZ8qSKzRTcFTgfqdoY4txdQrUhGHJGruXB",
+        pk: "edpkv6mD1SBoUB45g6Ss3LYfYzqZGxRfA3dehYBxEvGidFzsCg6yXQ",
+        peerId: "malloryPeerId"
+    },
+    oscar: {
+        pkh: "mv1N49LWf2GtvDBS2XGNZZEEKY4QNKxQjV4B",
+        sk: "edsk32TqRuUFWHE6jwrPgbk5M9A3Sbs4shY4dh1WJCMR1fJjoV6iNs",
+        pk: "edpkuTgScYTtrvAtTU6UMF44NvNd51ah1aqNeerNFC38bGZrKVXwFP",
+        peerId: "12D3KooWDgabT39cFp5j5mvJgiGPEppMuVgDCsNtBCh1Q8ejBCA5"
+    },
+    trudy: {
+        pkh: "mv1RmqZpXm6wQ45oTLMNKRVWSZBbCHN98bGe",
+        sk: "edsk3AbmsisVnZtkyVvY1jkyNpSTcRhW3hepihBLTu6e5sUaTS2x1c",
+        pk: "edpkubMsp1wdfpARu3ZsHKwrtkR9PQiAN8nsACP2hiksb7oM5aUhpu",
+        peerId: "12D3KooWLL2Y1JmrAXkY7r8xbuSRtasfJLAarXmAaZPYxPnzgAJ3"
+    },
+    isaac: {
+        pkh: "mv1Eb9L6u4MHkZ3ALdeXnJmAACZntu4R524m",
+        sk: "edsk3ahGaSWjzjHuS7mK5Bsy7LxH8iTRXpGcMvzjgSypKHKw2wrq1u",
+        pk: "edpktpYJesCTU55qxTy2RjdBUupRzVUM3Ek4VzVFxZjypLXPJSaTdn",
+        peerId: "isaacPeerId"
+    },
+    david: {
+        pkh: "mv1BkoZCjLwyH9wzHqzQkFUNPYA6wFzXKjTR",
+        sk: "edsk3hwth6tL9hppsUT6sZQ5687DDY9GPgKiZgjg9DDcMjJxoRUsGc",
+        pk: "edpkvK4zfvkuzHmbh2CofrBmQ52jrk3b23puVdJkTJCFmHZTLapSWB",
+        peerId: "davidPeerId"
+    },
+    susie: {
+        pkh: "mv1FmXyQkcfufUw9zwW3Dyg1W3JMHcDZ8rV5",
+        sk: "edsk2vtJ2rVoHoA3GbgDjyT5zbeVMDXZ6R4YjDskKaapgsRtiEWpaP",
+        pk: "edpkv6xHMqShSMr4CK3Vzg7mD1hina7eBkokrMoKpuYj55BVd18rjN",
+        peerId: "12D3KooWJQWBQvefFGj3uAzKGhpZYWYGKtj2fNQAG47aov4uj9p1"
+    },
+    ivan: {
+        pkh: "mv1PkouN3EwYmnfjFk817r7mXzTmjY1r5M1e",
+        sk: "edsk4AzUdwSFu383eMf8eve56Q2pJxy1eWt4BnzKkLKMdKurHgTeaf",
+        pk: "edpku8MxEyEKHPsPeUBSBz5a5QbemdPbiQVsobdC3SnC9UDf4yMHTL",
+        peerId: "ivanPeerId"
+    }
+};
